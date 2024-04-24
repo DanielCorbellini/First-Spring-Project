8da1838cb9dd755c6ceea4e4da3cc47f608a06e5
Daniel <daniel.corbellini28@gmail.com>
2024-04-24 10:41:03 -0300

    Creating objects from non-spring and from spring context

--- src/main/java/com/example/beans/Vehicle.java
+++ src/main/java/com/example/beans/Vehicle.java
@@ -2,14 +2,13 @@ package com.example.beans;
 
 public class Vehicle {
 
-	private String name;
+    private String name;
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 }
--- src/main/java/com/example/config/ProjectConfig.java
+++ src/main/java/com/example/config/ProjectConfig.java
@@ -5,14 +5,40 @@ import org.springframework.context.annotation.Configuration;
 
 import com.example.beans.Vehicle;
 
+/*
+	Spring @Configuration annotation is part of the spring core framework.
+	Spring Configuration annotation indicates that the class has @Bean definition
+	methods. So Spring container can process the class and generate Spring Beans
+	to be used in the application.
+*/
+
 @Configuration
 public class ProjectConfig {
 
+	/*
+	 * @Bean annotation, which lets Spring know that it needs to call this method
+	 * when it initializes its context and adds the returned value to the context.
+	 */
 	@Bean
 	Vehicle vehicle() {
 		var veh = new Vehicle();
 		veh.setName("Audi 8");
 		return veh;
+	}
+
+	/*
+	 * The method names usually follow verbs notation.But for methods which we will
+	 * use to create beans, can use nouns as names. This will be a good practise as
+	 * the method names will become bean names as well in the context.
+	 */
+	@Bean
+	String hello() {
+		return "Hello world";
+	}
+
+	@Bean
+	int number() {
+		return 16;
 	}
 
 }
