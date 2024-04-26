ba5148a7b4c11e4d6f387fadded46e0f1874c5ba
Daniel <daniel.corbellini28@gmail.com>
2024-04-26 10:08:42 -0300

    Understanding NoUniqueBeanDefinitionException

--- src/main/java/com/example/config/ProjectConfig.java
+++ src/main/java/com/example/config/ProjectConfig.java
@@ -1,9 +1,8 @@
 package com.example.config;
 
+import com.example.beans.Vehicle;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import com.example.beans.Vehicle;
 
 /*
 	Spring @Configuration annotation is part of the spring core framework.
@@ -15,30 +14,34 @@ import com.example.beans.Vehicle;
 @Configuration
 public class ProjectConfig {
 
-	/*
-	 * @Bean annotation, which lets Spring know that it needs to call this method
-	 * when it initializes its context and adds the returned value to the context.
-	 */
-	@Bean
-	Vehicle vehicle() {
-		var veh = new Vehicle();
-		veh.setName("Audi 8");
-		return veh;
-	}
+    /*
+     * @Bean annotation, which lets Spring know that it needs to call this method
+     * when it initializes its context and adds the returned value to the context.
+     */
+    @Bean
+    Vehicle vehicle1() {
+        var veh = new Vehicle();
+        veh.setName("Audi 8");
+        return veh;
+    }
 
-	/*
-	 * The method names usually follow verbs notation.But for methods which we will
-	 * use to create beans, can use nouns as names. This will be a good practise as
-	 * the method names will become bean names as well in the context.
-	 */
-	@Bean
-	String hello() {
-		return "Hello world";
-	}
+    @Bean
+    Vehicle vehicle2() {
+        var veh = new Vehicle();
+        veh.setName("Honda");
+        return veh;
+    }
 
-	@Bean
-	int number() {
-		return 16;
-	}
+    @Bean
+    Vehicle vehicle3() {
+        var veh = new Vehicle();
+        veh.setName("Porsche");
+        return veh;
+    }
 
+    /*
+     * The method names usually follow verbs notation.But for methods which we will
+     * use to create beans, can use nouns as names. This will be a good practise as
+     * the method names will become bean names as well in the context.
+     */
 }
--- src/main/java/com/example/main/Example1.java
+++ src/main/java/com/example/main/Example1.java
@@ -12,17 +12,20 @@ public class Example1 {
         System.out.println("Vehicle name from non-spring context is: " + vehicle.getName());
 
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-        Vehicle veh = context.getBean(Vehicle.class);
-        System.out.println("Vehicle name from Spring Context is: " + veh.getName());
+        Vehicle veh1 = context.getBean("vehicle1", Vehicle.class);
+        System.out.println("Vehicle1 name from Spring Context is: " + veh1.getName());
+
+        Vehicle veh2 = context.getBean("vehicle2", Vehicle.class);
+        System.out.println("Vehicle2 name from Spring Context is: " + veh2.getName());
+
+        Vehicle veh3 = context.getBean("vehicle3", Vehicle.class);
+        System.out.println("Vehicle3 name from Spring Context is: " + veh3.getName());
+
 
         /*
          * We don’t need to do any explicit casting while fetching a bean from context.
          * Spring is smart enough to look for a bean of the type you requested in its
          * context. If such a bean doesn’t exist,Spring will throw an exception.
          */
-        String hello = context.getBean(String.class);
-        System.out.println("String value from Spring Context is: " + hello);
-        Integer num = context.getBean(Integer.class);
-        System.out.println("Integer value from Spring Context is: " + num);
     }
 }
